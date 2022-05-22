8e4527a6ddaadb8cb5230846bf1565b37dd4ad92
Jose Mejia <mejialaguna@yahoo.com>
2022-05-21 20:24:49 -0400

    testing-nextJS-authentication

--- components/Header.js
+++ components/Header.js
@@ -7,12 +7,18 @@ import {
   PlayIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
-import { UserGroupIcon, ViewGridIcon } from "@heroicons/react/solid";
+import {
+  UserGroupIcon,
+  ViewGridIcon,
+  ChatIcon,
+  BellIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/solid";
 import { HeaderIcon } from "./HeaderIcon";
 
 export const Header = () => {
   return (
-    <div className=" bg-zinc-800 p-2 flex justify-between">
+    <div className="bg-zinc-800 p-2 flex justify-between">
       <div className="flex md:justify-between cursor-pointer">
         <Image
           className="cursor-pointer"
@@ -22,18 +28,18 @@ export const Header = () => {
           layout="fixed"
           alt="face-hook logo"
         />
-        <div className="flex ml-2 items-center bg-gray-100 rounded-full p-2 h-9 mt-1 ">
-          <SearchIcon className="h-6 items-center" />
+        <div className="flex ml-2 items-center bg-gray-100 rounded-full p-2 h-9 mt-1 bg-zinc-700">
+          <SearchIcon className="h-6 items-center text-white" />
           <input
-            className="hidden md:inline-flex ml-2 bg-transparent items-center outline-none placeholder-blue-500"
+            className="hidden md:inline-flex ml-2 bg-transparent items-center outline-none placeholder-zinc-400"
             type="text"
             placeholder="Search FaceHook..."
           />
         </div>
       </div>
 
-      <div className="mb-3" style={{ alignSelf: "flex-start" }}>
-        <div className="flex justify-between align-top">
+      <div className="mb-3 mt-3 md:mt-0">
+        <div className="flex space-x-6 md:-space-x-8 lg:space-x-0">
           <HeaderIcon active Icon={HomeIcon} />
           <HeaderIcon Icon={FlagIcon} />
           <HeaderIcon Icon={PlayIcon} />
@@ -42,12 +48,15 @@ export const Header = () => {
         </div>
       </div>
 
-      {/* <div>
-        <p className=" whitespace-nowrap text-2xl font-semibold  pr-3 text-white">
-          {" "}
+      <div className="hidden md:inline-flex items-center sm:space-x-2 justify-end">
+        <p className="whitespace-nowrap text-2xl font-semibold  pr-3 text-zinc-300">
           Jose Mejia
         </p>
-      </div> */}
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 };
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const HeaderIcon = ({ Icon , active }) => {
+export const HeaderIcon = ({ Icon, active }) => {
   return (
-    <div className="flex items-center cursor-pointer text-white md:px-10 md:text-white md:hover:bg-zinc-700 md:active:border-b-2 active:border-blue-500 md:hover:rounded-2xl  hover:text-blue-500 md:p-2 ">
-      <Icon className={`h-5 hover:animate-bounce md:hover:animate-none ${active && 'text-blue-500'}`} />
+    <div className="flex items-center cursor-pointer text-zinc-400 md:px-10  md:hover:bg-zinc-700 md:active:border-b-2 active:border-blue-500 md:hover:rounded-2xl  hover:text-blue-500 md:p-2 ">
+      <Icon
+        className={`h-5 hover:animate-bounce md:hover:animate-none ${
+          active && "text-blue-500"
+        }`}
+      />
     </div>
   );
 };
